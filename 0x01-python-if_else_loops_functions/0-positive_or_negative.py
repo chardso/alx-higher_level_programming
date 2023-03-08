b7245f194981a4d3cde1605c984bcc43024bfcc3
chardso <wafelaandrew2@gmail.com>
2023-03-09 01:36:20 +0300

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 /**
-*main-print if the number is positive,zero or negative
+* main - print if the number is positive,zero or negative
 * Return: Always (success)
 *
 */
@@ -13,13 +13,13 @@ int main (void)
         if (n > 0)
     {
 else if (n == 0)
-{
+}
         printf("%d is zero\n", n);
-}
-else
 {
-        printf("%d is negative\n", n);
+else
 }
+        printf("%d is negative\n", n);
+{
 return (0);
 }
  
