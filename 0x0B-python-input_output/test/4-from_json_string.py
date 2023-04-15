9c792fd2ca2a09f03f85e64f8a010df5bc36f391
chardso <wafelaandrew2@gmail.com>
2023-04-15 03:45:45 +0300

    0x0B-python input and output tasks mandatory

---
+++ 0x0B-python-input_output/test/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.8
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
+
