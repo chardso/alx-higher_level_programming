097ee6d62680c252edea0a2cdd36db90a90b0f2c
Chardson Andrea <chardsonandrea@gmail.com>
2023-07-13 18:40:34 +0300

    updates

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash/python3
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 curl -sb -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
 
