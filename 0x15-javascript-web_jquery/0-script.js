5c690c29289c0db15d8316f93914fe7df98c02fb
Chardson Andrea <chardsonandrea@gmail.com>
2023-08-04 11:10:32 +0300

    javascript jquery tasks

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+  * updates the color of the <header> element to red
+  */
+window.onload = function myFunction () {
+  const elem = document.querySelector('header');
+  elem.style.color = 'red';
+};
+
